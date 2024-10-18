5833760897f4277b7b3e81546d8ad1d26db9fc49
LoV432 <monib619@gmail.com>
2024-10-18 23:46:22 +0500

    add password matching

--- app/register/Form.tsx
+++ app/register/Form.tsx
@@ -28,7 +28,8 @@ export default function RegistrationForm() {
 		defaultValues: {
 			name: '',
 			email: '',
-			password: ''
+			password: '',
+			confirmPassword: ''
 		}
 	});
 
@@ -89,6 +90,19 @@ export default function RegistrationForm() {
 					render={({ field }) => (
 						<FormItem>
 							<FormLabel>Password</FormLabel>
+							<FormControl>
+								<Input type="password" placeholder="••••••••" {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="confirmPassword"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Confirm Password</FormLabel>
 							<FormControl>
 								<Input type="password" placeholder="••••••••" {...field} />
 							</FormControl>
--- app/register/FormSchema.z.ts
+++ app/register/FormSchema.z.ts
@@ -1,7 +1,13 @@
 import { z } from 'zod';
 
-export const registerFormSchema = z.object({
-	name: z.string().trim().min(1, 'Name is required'),
-	email: z.string().email('Invalid Email'),
-	password: z.string().min(10, 'The password must be minimum 10 characters')
-});
+export const registerFormSchema = z
+	.object({
+		name: z.string().trim().min(1, 'Name is required'),
+		email: z.string().email('Invalid Email'),
+		password: z.string().min(10, 'The password must be minimum 10 characters'),
+		confirmPassword: z.string().min(1, 'Please confirm your password')
+	})
+	.refine((data) => data.password === data.confirmPassword, {
+		message: "Passwords don't match",
+		path: ['confirmPassword']
+	});
