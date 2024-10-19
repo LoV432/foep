6002e4b70fc99ee0c71c7794260057d214e6ed46
LoV432 <monib619@gmail.com>
2024-10-19 21:09:01 +0500

    add register to homepage to debug

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ export default async function Home() {
 		<div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 sm:p-20">
 			<main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
 				<Link href="/login">Login</Link>
+				<Link href="/register">Register</Link>
 				<form action={destroySession}>
 					<button type="submit">Logout</button>
 				</form>
