1b163cf9a451eccc756e8b646801bc1a767ad4a9
LoV432 <monib619@gmail.com>
2024-10-25 01:41:23 +0500

    bigger preview image on phones

--- app/dashboard/UploadDialog.tsx
+++ app/dashboard/UploadDialog.tsx
@@ -68,17 +68,15 @@ export default function UploadDialog({
 								<div className="absolute right-0 top-0 flex h-full w-full flex-col gap-4 border-border bg-background p-4 md:relative md:border-l-2">
 									<h2 className="text-lg font-bold sm:text-xl">Details</h2>
 									<div className="flex flex-col gap-4">
-										<div className="mx-auto h-24 w-24 sm:h-32 sm:w-32 md:mx-0">
-											<Card className="h-full w-full">
-												<CardContent className="flex h-full w-full items-center justify-center p-0">
-													<MediaPreview
-														media={selectedMedia}
-														width={128}
-														height={128}
-													/>
-												</CardContent>
-											</Card>
-										</div>
+										<Card className="h-32 w-32 overflow-hidden">
+											<CardContent className="flex h-full w-full items-center justify-center p-0">
+												<MediaPreview
+													media={selectedMedia}
+													width={128}
+													height={128}
+												/>
+											</CardContent>
+										</Card>
 										<div className="flex flex-col gap-2">
 											<p className="text-sm font-bold">Name</p>
 											<p className="text-wrap text-sm">
